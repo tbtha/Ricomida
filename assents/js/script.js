185700da52bbb75493b6d56fa63e6a0ace7b2742
Tabatha Gamboa <tabathagamboa9@gmail.com>
2021-09-23 23:05:42 -0300

    Ricomida en proceso

---
+++ assents/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+$("#alertCorreo").click(function(){
+    $("button").hide();
+});
+});
